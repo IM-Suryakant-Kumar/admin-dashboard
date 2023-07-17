f4aa0fac7d47ca0ca36e08d292770b3d580c1dd7
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-07-17 18:38:17 +0530

    rendering data from data.json to App via material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
-import "./App.css";
+import "./styles/App.css";
+import TourCard from "./components/TourCard";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import SearchAppBar from "./components/AppBar";
+import cities from "./data.json";
+import { Typography } from "@mui/material";
 
 const App = () => {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	return (
+		<Box>
+			<SearchAppBar />
+			<Container sx={{ my: 5 }}>
+				{cities.map((city) => (
+					<>
+						<Typography
+							key={city.id}
+							variant="h4"
+							component="h2"
+							mt={5}
+							mb={3}
+						>
+							Top {city.name} Tours
+						</Typography>
+						<Grid
+							container
+							spacing={5}
+						>
+							{city.tours.map((tour) => (
+								<TourCard
+									key={tour.id}
+									tour={tour}
+								/>
+							))}
+						</Grid>
+					</>
+				))}
+			</Container>
+		</Box>
+	);
 };
 
 export default App;
