e176c3976f49d7dde6d4d582b604acba7420b3a0
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-07-18 19:32:04 +0530

    feat: Added Chart

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
+import Chart from "../components/Chart"
 import FeaturedInfo from "../components/FeaturedInfo"
 import { Container } from "../styles/home.sc"
+import userData from "../dummyData.json";
 
 const Home = () => {
   return (
     <Container component="section">
         <FeaturedInfo />
+        <Chart data={userData} title="User Analytics" dataKey="Active User" />
     </Container>
   )
 }
